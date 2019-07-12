08ab83aa86baa8d2b44c708283b97c4b6c756d98
biseki <asaiict1313@gmail.com>
2019-07-12 15:37:19 +0900

    [fix] 色々なおしたはず

--- arm.py
+++ arm.py
@@ -3,19 +3,19 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-gp_out = 4
-GPIO.setup(gp_out, GPIO.OUT)
-right = GPIO.PWM(gp_out, 50)
+gp_out1 = 4
+GPIO.setup(gp_out1, GPIO.OUT)
+right = GPIO.PWM(gp_out1, 50)
 right.start(0)
 
-gp_out =14
-GPIO.setup(gp_out, GPIO.OUT)
-left = GPIO.PWM(gp_out, 50)
+gp_out2 = 14
+GPIO.setup(gp_out2, GPIO.OUT)
+left = GPIO.PWM(gp_out2, 50)
 left.start(0)
 
 def default():
     right.ChangeDutyCycle(2.5)
-    left.ChangeDutyCycle(12)
+    left.ChangeDutyCycle(2.5)
 
 def oyasumi(): 
     right.ChangeDutyCycle(9.625)
@@ -38,9 +38,9 @@ def dance():
         right.ChangeDutyCycle(9.625)
         left.ChangeDutyCycle(12)
         time.sleep(0.5)
-    else:
-        print("Finish!")
 
+default()
+time.sleep(2)
 oyasumi()
 time.sleep(2)
 tadaima()
@@ -48,6 +48,5 @@ time.sleep(2)
 ohayou()
 time.sleep(2)
 dance()
-default()
 
 GPIO.cleanup()
