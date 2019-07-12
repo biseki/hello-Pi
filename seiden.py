0965957de9654cc619622ab941c84ef1c9d8ebfc
biseki <asaiict1313@gmail.com>
2019-07-12 15:15:26 +0900

    [fix]連続タッチにのみ反応するようにした

--- seiden.py
+++ seiden.py
@@ -9,10 +9,13 @@ print("start")
 
 i = 0
 
-while i < 2:
+while i < 1.8:
     if GPIO.input(26) != 0:
-        i += 1
-    time.sleep(1)
+        i += 0.1
+    else:
+        i = 0
+
+    time.sleep(0.1)
 
 GPIO.output(18, GPIO.HIGH)
 print("finish")
