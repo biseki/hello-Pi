69022f13da7efe264132977d3338f187ffe25ec7
biseki <asaiict1313@gmail.com>
2019-07-24 22:26:28 +0900

    たぶんまちがってる

--- seiden_arm.py
+++ seiden_arm.py
@@ -1,41 +1,43 @@
 import RPi.GPIO as GPIO
 import time
+class touchSensor:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(14, GPIO.IN)
 
-GPIO.setmode(GPIO.BCM)
+        GPIO.setup(24, GPIO.OUT)
+        right = GPIO.PWM(24, 50)
+        right.start(0)
 
-GPIO.setup(26, GPIO.IN)
+        GPIO.setup(23, GPIO.OUT)
+        left = GPIO.PWM(23, 50)
+        left.start(0)
 
-GPIO.setup(4, GPIO.OUT)
-right = GPIO.PWM(4, 50)
-right.start(0)
+    def dance(self):
+        for i in range(5):
+            right.ChangeDutyCycle(2.5)
+            left.ChangeDutyCycle(4.875)
+            time.sleep(0.5)
 
-GPIO.setup(14, GPIO.OUT)
-left = GPIO.PWM(14, 50)
-left.start(0)
+            right.ChangeDutyCycle(9.625)
+            left.ChangeDutyCycle(12)
+            time.sleep(0.5)
 
-def dance():
-    for i in range(5):
-        right.ChangeDutyCycle(2.5)
-        left.ChangeDutyCycle(4.875)
-        time.sleep(0.5)
+    print("start")
 
-        right.ChangeDutyCycle(9.625)
-        left.ChangeDutyCycle(12)
-        time.sleep(0.5)
 
-print("start")
-
-j = 0
-
-while j < 1.5:
-    if GPIO.input(26) != 0:
-        j += 0.1
-    else:
+    def touch_dance(self):
         j = 0
+        while j < 1.5:
+            if GPIO.input(14) != 0:
+                j += 0.1
+            else:
+                j = 0
 
-    time.sleep(0.1)
+            time.sleep(0.1)
+    dance()
 
-dance()
+touch_dance()
 print("finish")
 
 GPIO.cleanup()
