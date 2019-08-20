b745dd96d7471902806ef24efdd4ff011787e524
biseki <asaiict1313@gmail.com>
2019-08-20 23:02:14 +0900

    [change]静電容量センサから値を受け取るようにした

--- audio.py
+++ audio.py
@@ -3,13 +3,13 @@ import time
 import subprocess
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(26, GPIO.IN)
+GPIO.setup(14, GPIO.IN)
 
 CMD = "aplay /home/pi/hello-Pi/トーマス.wav"
 
 try:
     while True:
-        if GPIO.input(26) == GPIO.HIGH:
+        if GPIO.input(14) == GPIO.HIGH:
             subprocess.call(CMD, shell=True)
         time.sleep(0.01)
 
