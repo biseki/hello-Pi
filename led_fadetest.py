cd69dad664e237adc2511cec3ed908c6e780527e
biseki <asaiict1313@gmail.com>
2019-09-05 00:01:49 +0900

    [fix]黄色く光るようにした

--- led_fadetest.py
+++ led_fadetest.py
@@ -1,27 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 
-LedPin = 15
+rPin = 21
+gPin = 15
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LedPin, GPIO.OUT)
-GPIO.output(LedPin, GPIO.LOW)  
 
-p = GPIO.PWM(LedPin, 1000)
-p.start(0)
+GPIO.setup(rPin, GPIO.OUT)
+GPIO.output(rPin, GPIO.LOW)  
+r = GPIO.PWM(rPin, 1000)
+r.start(0)
+
+GPIO.setup(gPin, GPIO.OUT)
+GPIO.output(gPin, GPIO.LOW)  
+g = GPIO.PWM(gPin, 1000)
+g.start(0)
 
 try:
     while True:
         for dc in range(0, 101, 4):
-            p.ChangeDutyCycle(dc)
+            r.ChangeDutyCycle(dc)
+            g.ChangeDutyCycle(dc)
             time.sleep(0.05)
         time.sleep(1)
         for dc in range(100, -1, -4):
-            p.ChangeDutyCycle(dc)
+            r.ChangeDutyCycle(dc)
+            g.ChangeDutyCycle(dc)
             time.sleep(0.05)
         time.sleep(1)
 
 except KeyboardInterrupt:
-    p.stop()
-    GPIO.output(LedPin, GPIO.HIGH)
+    r.stop()
+    g.stop()
+    GPIO.output(rPin, GPIO.HIGH)
+    GPIO.output(gPin, GPIO.HIGH)
     GPIO.cleanup()
